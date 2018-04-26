82ddce98becbe013a495a9e22dff2e7624af4a87
Florian Tischler <florian.tischler@outlook.com>
2018-04-26 19:07:16 +0200

    changed benchmark extent

--- bench/access_benchmark.h
+++ bench/access_benchmark.h
@@ -6,12 +6,13 @@
 #include <SYCL/queue.h>
 #include <benchmark/benchmark.h>
 #include <boost/align/aligned_allocator.hpp>
-constexpr auto min_num_accessed = 1 << 25;
-constexpr auto num_elements = 1 << 28;
-constexpr auto multiplier = 2;
-constexpr auto repetitions = 1;
 #include <iostream>
 #include <vector>
+
+constexpr auto min_num_accessed = 1 << 21;
+constexpr auto num_elements = 1 << 25;
+constexpr auto multiplier = 2;
+constexpr auto repetitions = 10;
 
 template <class T>
 using aligned_vector = std::vector<T, boost::alignment::aligned_allocator<T, 64>>;
